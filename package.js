1b9e713022917ada2d469fea78e37e9fea07b92c
killuclkhioo-boop <killuclkhioo@gmail.com>
2026-04-16 15:34:07 +0700

    Create package.js

---
+++ package.js
@@ -0,0 +1,12 @@
+{
+  "name": "kuro-hosting-panel",
+  "version": "1.0.0",
+  "description": "Kuro Hosting Multi-Service Panel",
+  "main": "server.js",
+  "scripts": {
+    "start": "node server.js"
+  },
+  "dependencies": {
+    "express": "^4.18.2"
+  }
+}
